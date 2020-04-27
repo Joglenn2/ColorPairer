1a0f70bfb84331f7b04f31075d668c4dece9713b
Jack <joglenn2@illinois.edu>
2020-04-26 20:18:57 -0500

    Main view setup, Image importation working

--- app/src/main/java/com/example/colorpairer/MainActivity.java
+++ app/src/main/java/com/example/colorpairer/MainActivity.java
@@ -2,13 +2,60 @@ package com.example.colorpairer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+
+import com.skydoves.colorpickerview.ColorPickerView;
+import com.skydoves.colorpickerview.sliders.AlphaSlideBar;
+import com.skydoves.colorpickerview.sliders.BrightnessSlideBar;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
 
 public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_CODE_GALLERY = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ColorPickerView colorPickerView = findViewById(R.id.colorPickerView);
+
+        AlphaSlideBar alphaSlideBar = findViewById(R.id.alphaSlideBar);
+        colorPickerView.attachAlphaSlider(alphaSlideBar);
+
+        BrightnessSlideBar brightnessSlideBar = findViewById(R.id.brightnessSlide);
+        colorPickerView.attachBrightnessSlider(brightnessSlideBar);
+    }
+
+    public void imageFromGallery(View view) {
+        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+        photoPickerIntent.setType("image/*");
+        startActivityForResult(photoPickerIntent, REQUEST_CODE_GALLERY);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_GALLERY && resultCode == RESULT_OK && data != null) {
+            ColorPickerView colorPickerView = findViewById(R.id.colorPickerView);
+            try {
+                final Uri imageUri = data.getData();
+                final InputStream imageStream = getContentResolver().openInputStream(imageUri);
+                final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                Drawable drawable = new BitmapDrawable(getResources(), selectedImage);
+                colorPickerView.setPaletteDrawable(drawable);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
